6782bf28e5c705f3a9775d069a5157e5c57b5172
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-23 17:13:53 -0600

    added project mapping and updated css for backround header image

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Col, Container, Row, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 function Projects() {
+    const [projectList] = useState([
+        {
+            name:'Calculator',
+            github:'https://github.com/BenPaulat/personal-calculator-project',
+            deployment:'https://benpaulat.github.io/personal-calculator-project/',
+            description:'A simple, fully functional, personal calculator.',
+            image:'CalculatorScreenshot.png'
+        },
+        {
+            name:'Tennant',
+            github:'https://github.com/gunther123/Tennant',
+            deployment:'https://tennant-webapp-2.herokuapp.com/',
+            description:'A professional time tracking tool for a small business.',
+            image:'TennantScreenshot.png'
+        },
+        {
+            name:'Munchies & Movies',
+            github:'https://github.com/Ahmed-Sajjad111/Munchies-and-Movies',
+            deployment:'https://ahmed-sajjad111.github.io/Munchies-and-Movies/',
+            description:'A fun, stylized, web-application for searching movies and restaurants.',
+            image:'M&MScreenshot.png'
+        }
+    ])
+
+    /*
+    PROJECTS TEMPLATE
+    {
+        name:'',
+        github:'',
+        deployment:'',
+        description:'',
+        image:'../src/assets/'
+    }
+    */
     return (
-        <section>
-            <div>
-                <h2>A Few Things I've Worked On...</h2>
-                <div className='project-1'></div>
-                <div className='project-2'></div>
-                <div className='project-3'></div>
-                <div className='project-4'></div>
-                <div className='project-5'></div>
-            </div>
-            
-            
-        </section>
+        <div>
+            <Container>
+                <Row>
+                    {projectList.map((project) => {
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img 
+                                variant="top" 
+                                src={`../../src/assets/${project.image}`}
+                                alt={project.name}
+                                key={project.name}
+                            />
+                            <Card.Body>
+                                <Card.Title>{project.name}</Card.Title>
+                                <Card.Text>{project.description}</Card.Text>
+                            </Card.Body>
+                            <ListGroup className='list-group-flush'>
+                                <ListGroupItem>
+                                    <Card.Link href={project.github}>Github</Card.Link>
+                                    <Card.Link href={project.deployment}>Live Deployment</Card.Link>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card>
+                    })}
+                </Row>
+            </Container>
+        </div>
     );
 }
 
