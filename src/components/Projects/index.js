7d4932ade7436ff2ffbf8a4944b30bac07ab299c
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-24 02:28:59 -0600

    added readme file

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,46 +4,46 @@ import { Col, Container, Row, Card, ListGroup, ListGroupItem } from 'react-boots
 function Projects() {
     const [projectList] = useState([
         {
-            name:'Calculator',
-            github:'https://github.com/BenPaulat/personal-calculator-project',
-            deployment:'https://benpaulat.github.io/personal-calculator-project/',
-            description:'A simple, fully functional, personal calculator.',
-            image:'CalculatorScreenshot.PNG'
+            name: 'Calculator',
+            github: 'https://github.com/BenPaulat/personal-calculator-project',
+            deployment: 'https://benpaulat.github.io/personal-calculator-project/',
+            description: 'A simple, fully functional, personal calculator.',
+            image: 'CalculatorScreenshot.PNG'
         },
         {
-            name:'Tennant',
-            github:'https://github.com/gunther123/Tennant',
-            deployment:'https://tennant-webapp-2.herokuapp.com/',
-            description:'A professional time tracking tool for a small business.',
-            image:'TennantScreenshot.PNG'
+            name: 'Tennant',
+            github: 'https://github.com/gunther123/Tennant',
+            deployment: 'https://tennant-webapp-2.herokuapp.com/',
+            description: 'A professional time tracking tool for a small business.',
+            image: 'TennantScreenshot.PNG'
         },
         {
-            name:'Munchies & Movies',
-            github:'https://github.com/Ahmed-Sajjad111/Munchies-and-Movies',
-            deployment:'https://ahmed-sajjad111.github.io/Munchies-and-Movies/',
-            description:'A fun, stylized, web-application for searching movies and restaurants.',
-            image:'M&MScrenshot.PNG'
+            name: 'Munchies & Movies',
+            github: 'https://github.com/Ahmed-Sajjad111/Munchies-and-Movies',
+            deployment: 'https://ahmed-sajjad111.github.io/Munchies-and-Movies/',
+            description: 'A fun, stylized, web-application for searching movies and restaurants.',
+            image: 'M&MScrenshot.PNG'
         },
         {
-            name:'Employee Database',
-            github:'https://github.com/BenPaulat/Employee-Database',
-            deployment:'_blank',
-            description:'A command line program to manage a small employee database.',
-            image:'EmployeeDBScreenshot.png'
+            name: 'Employee Database',
+            github: 'https://github.com/BenPaulat/Employee-Database',
+            deployment: '_blank',
+            description: 'A command line program to manage a small employee database.',
+            image: 'EmployeeDBScreenshot.png'
         },
         {
-            name:'Budget Tracker',
-            github:'https://github.com/BenPaulat/budget-tracker',
-            deployment:'https://pacific-shore-79406.herokuapp.com/',
-            description:'A simple budget tracking app that works on the go!',
-            image:'BudgetTracker.PNG'
+            name: 'Budget Tracker',
+            github: 'https://github.com/BenPaulat/budget-tracker',
+            deployment: 'https://pacific-shore-79406.herokuapp.com/',
+            description: 'A simple budget tracking app that works on the go!',
+            image: 'BudgetTracker.PNG'
         },
         {
-            name:'Workday Scheduler',
-            github:'https://github.com/BenPaulat/day-planner',
-            deployment:'https://benpaulat.github.io/day-planner/',
-            description:'A simple calender app for scheduling your work day.',
-            image:'WorkdayScheduler.PNG'
+            name: 'Workday Scheduler',
+            github: 'https://github.com/BenPaulat/day-planner',
+            deployment: 'https://benpaulat.github.io/day-planner/',
+            description: 'A simple calender app for scheduling your work day.',
+            image: 'WorkdayScheduler.PNG'
         }
     ])
 
@@ -63,8 +63,8 @@ function Projects() {
                 <Row>
                     {projectList.map((project) => (
                         <Card key={project.name} style={{ width: '18rem' }}>
-                            <Card.Img 
-                                variant="top" 
+                            <Card.Img
+                                variant="top"
                                 src={require(`../../assets/project-screenshots/${project.image}`)}
                                 alt={project.name}
                                 className='card-image'
