90540cd9f6013e169e04d0dfd6ab9722c8922762
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-24 02:41:03 -0600

    updated footer styling again

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 function Footer() {
     return (
         <footer className="fixed-bottom page-footer font-small py-2">
-            <div className='footer-container'>
-                <div>
-                    <a className='btn-fb mx-1' href='https://github.com/BenPaulat/'>
+            <Row className='footer-container'>
+                <Col>
+                    <a className='btn-fb mx-1 link' href='https://github.com/BenPaulat/'>
                         Visit My Github
                     </a>
-                </div>
-                <div>
-                    <a className='btn-fb mx-1' href='https://linkedin.com/in/benjamin-paulat-77165a123'>
+                </Col>
+                <Col>
+                    <a className='btn-fb mx-1 link' href='https://linkedin.com/in/benjamin-paulat-77165a123'>
                         Check Out My LinkedIn
                     </a>
-                </div>
-                <div>
+                </Col>
+                <Col>
                     Made with ☕ and ❤ by Benjamin Paulat
-                </div>
-            </div>
+                </Col>
+            </Row>
         </footer>
         
     );
