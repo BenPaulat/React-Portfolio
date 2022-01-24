58705f38db0d36c66078046a449ded09ae2b66cd
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-23 19:39:09 -0600

    updated footer and implemented Font Awesome

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Stack } from 'react-bootstrap';
 
 function Footer() {
     return (
-        <footer>Made with ☕ and ❤ by Benjamin Paulat</footer>
+        <footer className="fixed-bottom">
+            <Stack direction="horizontal" gap={3}>
+                <div>
+                    <a className='fab fa-github' href='https://github.com/BenPaulat/'></a>
+                </div>
+                <div>
+                    <a className='fab fa-linkedin' href='https://linkedin.com/in/benjamin-paulat-77165a123'></a>
+                </div>
+                <div>
+                    Made with ☕ and ❤ by Benjamin Paulat
+                </div>
+            </Stack>
+        </footer>
         
     );
 }
