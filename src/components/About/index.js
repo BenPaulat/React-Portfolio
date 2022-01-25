82352e03d196b6d920edcbf341a13c48009379f8
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-24 23:05:59 -0600

    removed erroreous about me test section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 function AboutMe() {
     return (
         <div>
-            <h2>About Me Test</h2>
             <section>
                 Hello, and thank you for checking out my portfolio! I am Ben and am an aspiring Web Engineer. I have experience in IT Support and Service, Customer Service, and Team Organization and Management. Please take a look at my developer projects and consider me as a future resource for your coding needs!
             </section>
